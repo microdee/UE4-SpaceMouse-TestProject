b479b91059760bb9e9fc3ee42d0ca9cd9c8295b4
David Morasz <m@mcro.de>
2021-06-12 13:19:14 +0200

    + UE5 deployment

--- DeployPlugin.ps1
+++ DeployPlugin.ps1
@@ -4,3 +4,6 @@
 .\build.cmd --target MakeRelease --for-marketplace --unreal-version 4.24.3 --no-logo
 .\build.cmd --target MakeRelease --for-marketplace --unreal-version 4.25.4 --no-logo
 .\build.cmd --target MakeRelease --for-marketplace --unreal-version 4.26.2 --no-logo
+.\build.cmd --target MakeRelease --for-marketplace --unreal-version 4.27.0 --no-logo
+
+.\build.cmd --target MakeRelease --custom-engine-path "C:\EpicGames\UE_5.0EA" --unreal-version 5.0.0 --no-logo
