817be92d8dc04282120acbcca37fdbbfda641981
David Morasz <m@mcro.de>
2022-08-14 15:07:01 +0200

    update to 1.2.5

--- DeployPlugin.ps1
+++ DeployPlugin.ps1
@@ -1,9 +1,8 @@
 
-.\build.cmd --target CleanDeployment --no-logo
-.\build.cmd --target MakeRelease --unreal-version 4.23.1 --no-logo
-.\build.cmd --target MakeRelease --for-marketplace --unreal-version 4.24.3 --no-logo
-.\build.cmd --target MakeRelease --for-marketplace --unreal-version 4.25.4 --no-logo
-.\build.cmd --target MakeRelease --for-marketplace --unreal-version 4.26.2 --no-logo
-.\build.cmd --target MakeRelease --for-marketplace --unreal-version 4.27.0 --no-logo
-
-.\build.cmd --target MakeRelease --unreal-version 5.0.0EA --no-logo
+.\build.cmd --target CleanDeployment
+.\build.cmd --target MakeRelease --unreal-version 4.23.1
+.\build.cmd --target MakeRelease --for-marketplace --unreal-version 4.24.3
+.\build.cmd --target MakeRelease --for-marketplace --unreal-version 4.25.4
+.\build.cmd --target MakeRelease --for-marketplace --unreal-version 4.26.2
+.\build.cmd --target MakeRelease --for-marketplace --unreal-version 4.27.2
+.\build.cmd --target MakeRelease --for-marketplace --unreal-version 5.0.3
--- Nuke.Targets/Build.cs
+++ Nuke.Targets/Build.cs
@@ -3,6 +3,7 @@ using Nuke.Unreal;
 class Build : UnrealBuild, IPluginTargets
 {
     public static int Main() => Execute<Build>(x => x.BuildEditor);
+    protected override void OnBuildCreated() => NoLogo = true;
     
-    public string PluginVersion => "1.2.4";
+    public string PluginVersion => "1.2.5";
 }
