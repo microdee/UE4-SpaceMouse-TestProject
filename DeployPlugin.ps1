fc5b3b9e1fe77c4082e714f346e03bc09d72da88
David Morasz <m@mcro.de>
2022-08-14 16:08:57 +0200

    update SpaceMouse plugin to 1.2.5

--- DeployPlugin.ps1
+++ DeployPlugin.ps1
@@ -1,6 +1,5 @@
 
 .\build.cmd --target CleanDeployment
-.\build.cmd --target MakeRelease --unreal-version 4.23.1
 .\build.cmd --target MakeRelease --for-marketplace --unreal-version 4.24.3
 .\build.cmd --target MakeRelease --for-marketplace --unreal-version 4.25.4
 .\build.cmd --target MakeRelease --for-marketplace --unreal-version 4.26.2
