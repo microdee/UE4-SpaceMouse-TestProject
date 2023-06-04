1b36967750a2296f9a5c2d17c9bd07f18b24d4ab
David Mórász <m@mcro.de>
2023-06-04 16:24:06 +0200

    remove old Nuke

--- Nuke.Targets/Build.cs
+++
@@ -1,9 +0,0 @@
-using Nuke.Unreal;
-
-class Build : UnrealBuild, IPluginTargets
-{
-    public static int Main() => Execute<Build>(x => x.BuildEditor);
-    protected override void OnBuildCreated() => NoLogo = true;
-    
-    public string PluginVersion => "1.2.5";
-}
