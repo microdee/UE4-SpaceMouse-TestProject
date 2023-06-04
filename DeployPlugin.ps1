35c6ab646b65823c776b04c2fa0039c648e0d392
David Mórász <m@mcro.de>
2023-06-04 18:59:11 +0200

    updating supported Unreal versions

--- DeployPlugin.ps1
+++ DeployPlugin.ps1
@@ -1,6 +1,6 @@
 
-.\build.cmd --target CleanDeployment
-.\build.cmd --target MakeRelease --for-marketplace --unreal-version 4.27.2
-.\build.cmd --target MakeRelease --for-marketplace --unreal-version 5.0.3
-.\build.cmd --target MakeRelease --for-marketplace --unreal-version 5.1.1
-.\build.cmd --target MakeRelease --for-marketplace --unreal-version 5.2.0
+.\build.cmd CleanDeployment
+.\build.cmd MakeRelease --for-marketplace --unreal-version 4.27.2
+.\build.cmd MakeRelease --for-marketplace --unreal-version 5.0.3
+.\build.cmd MakeRelease --for-marketplace --unreal-version 5.1.1
+.\build.cmd MakeRelease --for-marketplace --unreal-version 5.2.0
